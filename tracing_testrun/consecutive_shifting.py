e8c2b8376ef6cc69cd10a90c001f82e607ad28e1
chadrick-kwag <chadrick.kwag@gmail.com>
2019-05-27 08:06:57 +0900

    fix float coord error
    - save initial starting box

--- tracing_testrun/consecutive_shifting.py
+++ tracing_testrun/consecutive_shifting.py
@@ -17,6 +17,17 @@ def predict_for_n_runs_and_save_movement(start_coord, full_img, savedir, predict
     smaller_dim = min(full_h, full_w)
     move_unit = int(smaller_dim * 0.02)
 
+
+    # draw start box
+    copyimg = full_img.copy()
+    p1 = tuple(coord[:2])
+    p2 = tuple(coord[2:])
+
+    cv2.rectangle(copyimg, p1,p2, (0,0,255),2)
+    savepath = os.path.join(savedir, "start.png")
+    cv2.imwrite(savepath, copyimg)
+
+
     for i in range(repeat_num):
         x1,y1,x2,y2 = coord
         print(f"iter:{i:02d}, coord={coord}")
@@ -31,6 +42,7 @@ def predict_for_n_runs_and_save_movement(start_coord, full_img, savedir, predict
         dy = -(move_unit * np.sin(angle_rad))  # use negative since y axis direction of cv2 coord is.
 
         coord = shift_coord(coord, dx,dy)
+        coord = [ int(x) for x in coord ] 
 
         # draw the new coord
         copyimg = full_img.copy()
